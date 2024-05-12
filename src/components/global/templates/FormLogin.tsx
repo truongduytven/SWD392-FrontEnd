c3b443a6aad6eabfff584bb7b7ec04bd550b579f
ThuongMinhMai <thuongmtmse170103@fpt.edu.vn>
2024-05-12 23:55:31 +0700

    fix form

--- src/components/global/templates/FormLogin.tsx
+++ src/components/global/templates/FormLogin.tsx
@@ -1,57 +1,47 @@
-import React,{useState} from 'react'
-
+import React, { useState } from 'react'
+import './FormLogin.css'
 function FormLogin() {
-  
-  
+  const [addclass, setaddclass] = useState('')
   return (
-      <div className="container" id="container">
-        <div className="form-container sign-up">
-            <form>
-                <h1>Create Account</h1>
-                <div className="social-icons">
-                    <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
-                    <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
-                    <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
-                    <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
-                </div>
-                <span>or use your email for registeration</span>
-                <input type="text" placeholder="Name"/>
-                <input type="email" placeholder="Email"/>
-                <input type="password" placeholder="Password"/>
-                <button>Sign Up</button>
-            </form>
+    <div className=''>
+
+    <div className={`container ${addclass}`} id='container'>
+      <div className='form-container sign-up-container'>
+        <form>
+          <h1>Create Account</h1>
+          <input type='text' placeholder='NAME' />
+          <input type='email' placeholder='EMAIL' />
+          <input type='password' placeholder='PASSWORD' autoComplete='on' />
+          <button type='submit'>REGISTER</button>
+        </form>
+      </div>
+      <div className='form-container sign-in-container'>
+        <form>
+          <h1>Login</h1>
+          <input type='email' placeholder='EMAIL' className='w-full' />
+          <input type='password' placeholder='PASSWORD' autoComplete='on' />
+          <button type='submit'>LOGIN</button>
+        </form>
+      </div>
+      <div className='overlay-container'>
+        <div className='overlay'>
+          <div className='overlay-panel overlay-left'>
+            {addclass &&<button className='ghost' id='signIn' onClick={() => setaddclass('')}>
+              GO TO LOGIN
+            </button>}
+            
+          </div>
+          <div className='overlay-panel overlay-right'>
+            {!addclass && <button className='ghost' id='signUp' onClick={() => setaddclass('right-panel-active')}>
+              GO TO REGISTER
+            </button>}
+            
+          </div>
         </div>
-        <div className="form-container sign-in">
-            <form>
-                <h1>Sign In</h1>
-                <div className="social-icons">
-                    <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
-                    <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
-                    <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
-                    <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
-                </div>
-                <span>or use your email password</span>
-                <input type="email" placeholder="Email"/>
-                <input type="password" placeholder="Password"/>
-                <a href="#">Forget Your Password?</a>
-                <button>Sign In</button>
-            </form>
-        </div>
-        <div className="toggle-container">
-            <div className="toggle">
-                <div className="toggle-panel toggle-left">
-                    <h1>Welcome Back!</h1>
-                    <p>Enter your personal details to use all of site features</p>
-                    <button className="hidden" id="login">Sign In</button>
-                </div>
-                <div className="toggle-panel toggle-right">
-                    <h1>Hello, Friend!</h1>
-                    <p>Register with your personal details to use all of site features</p>
-                    <button id="register" className=''>Sign Up</button>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
+    </div>
+
   )
 }
 
