b168bed0eb17e79e1ce43284f7b89344a45e5608
ThuongMinhMai <thuongmtmse170103@fpt.edu.vn>
2024-05-13 12:38:00 +0700

    fix:formLogin

--- src/components/global/templates/FormLogin.tsx
+++ src/components/global/templates/FormLogin.tsx
@@ -1,57 +1,46 @@
-
 import React, { useState } from 'react'
+import { Input } from "@/components/global/atoms/input"
+
 import './FormLogin.css'
 function FormLogin() {
-
-
   const [addclass, setaddclass] = useState('')
   return (
-    <div className='h-screen flex justify-center items-center'>
-
-    <div className={`container ${addclass}`} id="container">
-    <div className="form-container  sign-up-container">
-      <form>
-        <h1>Create Account</h1>
-        <input type="text" placeholder="NAME" />
-        <input type="email" placeholder="EMAIL" />
-        <input type="password" placeholder="PASSWORD" />
-        <input type="password" placeholder="CONFIRM PASSWORD" />
-        <button type="submit">REGISTER</button>
-      </form>
-    </div>
-    <div className="form-container sign-in-container">
-      <form>
-        <h1>Login</h1>
-        <input type="email" placeholder="EMAIL" />
-        <input type="password" placeholder="PASSWORD" />
-        <button type="submit">LOGIN</button>
-      </form>
-    </div>
-    <div className="overlay-container">
-      <div className="overlay">
-        <div className="overlay-panel overlay-left">
-          <button
-            className="ghost"
-            id="signIn"
-            onClick={() => setaddclass("")}
-          >
-            GO TO LOGIN
-          </button>
+    <div className='h-screen flex justify-center items-center  '>
+      <div className={`container ${addclass}`} id='container'>
+        <div className='form-container  sign-up-container'>
+          <form className='bg-white flex items-center justify-center flex-col px-10 h-full text-center shadow-lg'>
+            <h1>Create Account</h1>
+            <Input type='text' placeholder='NAME' />
+            <Input type='email' placeholder='EMAIL' />
+            <Input type='password' placeholder='PASSWORD' />
+            <Input type='password' placeholder='CONFIRM PASSWORD' />
+            <button type='submit'>REGISTER</button>
+          </form>
         </div>
-        <div className="overlay-panel overlay-right">
-          <button
-            className="ghost"
-            id="signUp"
-            onClick={() => setaddclass("right-panel-active")}
-          >
-            GO TO REGISTER
-          </button>
+        <div className='form-container sign-in-container'>
+          <form className='bg-white flex items-center justify-center flex-col px-10 h-full text-center shadow-lg'>
+            <h1>Login</h1>
+            <Input type='email' placeholder='EMAIL' />
+            <Input type='password' placeholder='PASSWORD' />
+            <button type='submit'>LOGIN</button>
+          </form>
+        </div>
+        <div className='overlay-container '>
+          <div className='overlay'>
+            <div className='overlay-panel overlay-left'>
+              <button className='ghost' id='signIn' onClick={() => setaddclass('')}>
+                GO TO LOGIN
+              </button>
+            </div>
+            <div className='overlay-panel overlay-right'>
+              <button className='ghost' id='signUp' onClick={() => setaddclass('right-panel-active')}>
+                GO TO REGISTER
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  </div>
-
   )
 }
 
