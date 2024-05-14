7ca29bb9ff6c736f3383976675c718169e54eb4e
Võ Nhật Trường Duy K17 <duyvntse170126@fpt.edu.vn>
2024-05-14 10:26:59 +0700

    style: fix Logo

--- src/components/global/organisms/Header.tsx
+++ src/components/global/organisms/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="h-64 bg-gradient-to-b from-yellow-200 to-primary relative">
         <div className="w-full h-1/3 flex justify-center">
-            <div className="px-5 w-1/5 h-full bg-secondary rounded-bl-full rounded-br-full shadow-lg">
+            <div className="px-6 w-1/5 h-full bg-secondary rounded-bl-full rounded-br-full shadow-lg">
                 <img src={LogoFull} alt="Logo"/>
             </div>
         </div>
