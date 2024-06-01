97e0a01e355157dac131a4531c8a7767bb31dce8
ThuongMinhMai <thuongmtmse170103@fpt.edu.vn>
2024-06-01 08:23:57 +0700

    fix:login button

--- src/components/global/organisms/Header.tsx
+++ src/components/global/organisms/Header.tsx
@@ -64,7 +64,7 @@ function Header() {
               {/* <Button variant="ghost" className="relative text-black hover:text-primary cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-primary before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-primary after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
                 Đăng ký/Đăng nhập
               </Button> */}
-              <button className='text-black font-medium hover:before:bg-redborder-red-500 relative h-fit py-2 w-fit overflow-hidden  bg-white px-3 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-orange-500 before:transition-all before:duration-500 hover:text-white  hover:before:left-0 hover:before:w-full'>
+              <button className='text-black font-medium hover:before:bg-redborder-red-500 relative h-fit py-2 w-fit overflow-hidden  bg-white px-3 transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-orange-500 before:transition-all before:duration-500 hover:text-white  hover:before:left-0 hover:before:w-full'>
                 <p className='relative z-10'> Đăng ký/Đăng nhập</p>
               </button>
             </Link>
