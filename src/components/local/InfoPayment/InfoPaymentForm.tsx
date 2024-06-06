7f192f53948d4af5f840e186d3ab095664cc63da
Võ Nhật Trường Duy K17 <duyvntse170126@fpt.edu.vn>
2024-06-06 23:40:47 +0700

    style: choose service no choose seat

---
+++ src/components/local/InfoPayment/InfoPaymentForm.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Button } from "@/components/global/atoms/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/global/atoms/form"
+import { Input } from "@/components/global/atoms/input"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.'
+  })
+})
+
+function InfoPaymentForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: ''
+    }
+  })
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder='shadcn' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default InfoPaymentForm
