03483fca383ef2b46a58b36d352a15133d26d085
ThuongMinhMai <thuongmtmse170103@fpt.edu.vn>
2024-06-12 20:14:26 +0700

    fix:fetch picture

--- src/components/global/organisms/CardTrip.tsx
+++ src/components/global/organisms/CardTrip.tsx
@@ -127,7 +127,7 @@ function CardTrip({ data }: ITripDataProps) {
               <TabsTrigger value='danhgia'>Đánh giá</TabsTrigger>
             </TabsList>
             <TabsContent value='hinhanh'>
-              <div className='p-4 flex flex-col justify-center items-center'>
+              {/* <div className='p-4 flex flex-col justify-center items-center'>
                 <div className='relative overflow-hidden mb-4 w-full h-96'>
                   <div
                     className='flex transition-transform duration-500'
@@ -163,7 +163,52 @@ function CardTrip({ data }: ITripDataProps) {
                     />
                   ))}
                 </div>
+              </div> */}
+                {isLoading ? (
+            <p>Loading details...</p>
+          ) : error ? (
+            <p>Error loading details</p>
+          ) : (
+            tripPictureDetails && tripPictureDetails.length > 0 && (
+              <div className='p-4 flex flex-col justify-center items-center'>
+                <div className='relative overflow-hidden mb-4 w-full h-96'>
+                  <div
+                    className='flex transition-transform duration-500'
+                    style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+                  >
+                    {tripPictureDetails.map((image: string, index: number) => (
+                      <div key={index} className='flex-none w-full h-96'>
+                        <img src={image} alt={`Slide ${index}`} className='w-full h-full rounded-md object-cover' />
+                      </div>
+                    ))}
+                  </div>
+                  <button
+                    onClick={handlePrev}
+                    className='absolute left-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-75 p-1 flex justify-center items-center rounded-full shadow hover:bg-opacity-100 transition'
+                  >
+                    <ChevronLeft className='text-primary'/>
+                  </button>
+                  <button
+                    onClick={handleNext}
+                    className='absolute right-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-75 p-1 rounded-full shadow hover:bg-opacity-100 transition'
+                  >
+                    <ChevronRight className='text-primary' />
+                  </button>
+                </div>
+                <div className='flex space-x-4 overflow-x-auto'>
+                  {tripPictureDetails.map((image: string, index: number) => (
+                    <img
+                      key={index}
+                      src={image}
+                      alt={`Thumbnail ${index}`}
+                      className={`w-24 h-24 object-cover rounded-md cursor-pointer ${currentIndex === index ? 'border-2 border-primary' : ''}`}
+                      onClick={() => setCurrentIndex(index)}
+                    />
+                  ))}
+                </div>
               </div>
+            )
+          )}
             </TabsContent>
             <TabsContent value='tienich'>Tiện ích đâu.</TabsContent>
             <TabsContent value='lotrinh'>
