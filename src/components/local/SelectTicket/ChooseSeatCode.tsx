89e1f358d24562160968a36714ab41024df1530a
Võ Nhật Trường Duy K17 <duyvntse170126@fpt.edu.vn>
2024-06-23 22:38:07 +0700

    fix: retype seatdata

--- src/components/local/SelectTicket/ChooseSeatCode.tsx
+++ src/components/local/SelectTicket/ChooseSeatCode.tsx
@@ -1,12 +1,12 @@
 import SeatLayout from "@/components/local/SelectTicket/SeatCodeLayout"
+import { defaultSeats } from "@/constants/SeatData"
+import { formatPrice } from "@/lib/utils"
 
 function ChooseSeatCode() {
+  const Colors = ['bg-tertiary', 'bg-pink-400', 'bg-blue-400']
   return (
-    <div className='flex flex-col justify-between p-4 h-full'>
-      <div className="flex justify-evenly text-center h-full mb-4 space-x-3">
-        <SeatLayout />
-      </div>
-      <div className='flex items-center justify-evenly'>
+    <div className='flex justify-around p-4 h-full'>
+      <div className='flex flex-col items-start justify-center space-y-10 mt-10'>
         <div className='flex items-center space-x-3 text-sm'>
           <div className='h-5 w-5 rounded-sm bg-gray-300' />
           <span>Đã bán</span>
@@ -15,14 +15,15 @@ function ChooseSeatCode() {
           <div className='h-5 w-5 rounded-sm bg-red-500' />
           <span>Đang chọn</span>
         </div>
-        <div className='flex items-center space-x-3 text-sm'>
-          <div className='h-5 w-5 rounded-sm bg-tertiary' />
-          <span>Ghế còn trống - 400.000đ</span>
-        </div>
-        {/* <div className='flex items-center space-x-3 text-sm'>
-          <div className='h-5 w-5 rounded-sm bg-pink-500' />
-          <span>Ghế hàng cuối - 120.000đ</span>
-        </div> */}
+        {defaultSeats.map((seat, index) => (
+          <div key={index} className='flex items-center space-x-3 text-sm'>
+            <div className={`h-5 w-5 rounded-sm ${Colors[index]}`} />
+            <span>{seat.TicketTypeName === 'front' ? 'Hàng trên' : seat.TicketTypeName === 'back' ? 'Hàng cuối' : 'Hàng giữa'} - {formatPrice(seat.price)}</span>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-evenly text-center h-full mb-4 space-x-3">
+        <SeatLayout />
       </div>
     </div>
   )
--- src/constants/SeatData.ts
+++ src/constants/SeatData.ts
@@ -1,8 +1,15 @@
 // defaultSeatData.ts
-export interface Seat {
-    seatCode: string;
+// export interface Seat {
+//     seatCode: string;
+//     price: number;
+//     status: string;
+//   }
+
+  export interface Seat {
+    TicketTypeId: string;
+    TicketTypeName: string;
     price: number;
-    status: string;
+    quantity: number;
   }
   
 export interface ServiceDefault {
@@ -25,27 +32,48 @@ export const stationData: string[] = [
 ]
 
 export const defaultSeats: Seat[] = [
-    { seatCode: 'A01', price: 400000, status: 'bought' },
-    { seatCode: 'A02', price: 400000, status: 'bought' },
-    { seatCode: 'A03', price: 400000, status: 'bought' },
-    { seatCode: 'A04', price: 400000, status: 'notsold' },
-    { seatCode: 'A05', price: 400000, status: 'notsold' },
-    { seatCode: 'A06', price: 400000, status: 'notsold' },
-    { seatCode: 'A07', price: 400000, status: 'notsold' },
-    { seatCode: 'A08', price: 400000, status: 'notsold' },
-    { seatCode: 'A09', price: 400000, status: 'notsold' },
-    { seatCode: 'A10', price: 400000, status: 'notsold' },
-    { seatCode: 'B01', price: 400000, status: 'bought' },
-    { seatCode: 'B02', price: 400000, status: 'bought' },
-    { seatCode: 'B03', price: 400000, status: 'bought' },
-    { seatCode: 'B04', price: 400000, status: 'notsold' },
-    { seatCode: 'B05', price: 400000, status: 'notsold' },
-    { seatCode: 'B06', price: 400000, status: 'notsold' },
-    { seatCode: 'B07', price: 400000, status: 'notsold' },
-    { seatCode: 'B08', price: 400000, status: 'notsold' },
-    { seatCode: 'B09', price: 400000, status: 'notsold' },
-    { seatCode: 'B10', price: 400000, status: 'notsold' },
-  ];
+  {
+    TicketTypeId: '1',
+    TicketTypeName: 'front',
+    price: 100000,
+    quantity: 12,
+  },
+  {
+    TicketTypeId: '2',
+    TicketTypeName: 'middle',
+    price: 120000,
+    quantity: 8,
+  },
+  {
+    TicketTypeId: '3',
+    TicketTypeName: 'back',
+    price: 140000,
+    quantity: 12,
+  },
+];
+
+// export const defaultSeats: Seat[] = [
+//     { seatCode: 'A01', price: 400000, status: 'bought' },
+//     { seatCode: 'A02', price: 400000, status: 'bought' },
+//     { seatCode: 'A03', price: 400000, status: 'bought' },
+//     { seatCode: 'A04', price: 400000, status: 'notsold' },
+//     { seatCode: 'A05', price: 400000, status: 'notsold' },
+//     { seatCode: 'A06', price: 400000, status: 'notsold' },
+//     { seatCode: 'A07', price: 400000, status: 'notsold' },
+//     { seatCode: 'A08', price: 400000, status: 'notsold' },
+//     { seatCode: 'A09', price: 400000, status: 'notsold' },
+//     { seatCode: 'A10', price: 400000, status: 'notsold' },
+//     { seatCode: 'B01', price: 400000, status: 'bought' },
+//     { seatCode: 'B02', price: 400000, status: 'bought' },
+//     { seatCode: 'B03', price: 400000, status: 'bought' },
+//     { seatCode: 'B04', price: 400000, status: 'notsold' },
+//     { seatCode: 'B05', price: 400000, status: 'notsold' },
+//     { seatCode: 'B06', price: 400000, status: 'notsold' },
+//     { seatCode: 'B07', price: 400000, status: 'notsold' },
+//     { seatCode: 'B08', price: 400000, status: 'notsold' },
+//     { seatCode: 'B09', price: 400000, status: 'notsold' },
+//     { seatCode: 'B10', price: 400000, status: 'notsold' },
+//   ];
 
   export const ServiceData: ServiceDefault[] = [
     {
