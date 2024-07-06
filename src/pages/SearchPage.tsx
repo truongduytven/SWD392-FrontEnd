5ffe673360b82c471240aab6474d8aa7ea64a113
ThuongMinhMai <thuongmtmse170103@fpt.edu.vn>
2024-07-06 15:17:13 +0700

    fix:loading

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,126 +1,122 @@
-import { useGetCompanies } from '@/apis/companyAPI'
-import { useGetCitySearchForm, useGetTripSearchForm } from '@/apis/tripAPI'
-import Loading from '@/components/global/molecules/Loading'
-import CardTrip from '@/components/global/organisms/CardTrip'
-import { SearchForm } from '@/components/local/Search/SearchForm'
-import Arrange from '@/components/local/filter/Arrange'
-import BadgeList from '@/components/local/filter/BadgeListFilter'
-import BusFilter from '@/components/local/filter/BusFilter'
-import TypeFilter from '@/components/local/filter/TypeFilter'
-import { SearchData, useSearch } from '@/contexts/SearchContext'
-import { findCityNameByID } from '@/lib/utils'
-import { ArrowBigUpDash, Trash2 } from 'lucide-react'
-import React, { useEffect, useState } from 'react'
+import { useGetCompanies } from '@/apis/companyAPI';
+import { useGetCitySearchForm, useGetTripSearchForm } from '@/apis/tripAPI';
+import Loading from '@/components/global/molecules/Loading';
+import CardTrip from '@/components/global/organisms/CardTrip';
+import { SearchForm } from '@/components/local/Search/SearchForm';
+import Arrange from '@/components/local/filter/Arrange';
+import BadgeList from '@/components/local/filter/BadgeListFilter';
+import BusFilter from '@/components/local/filter/BusFilter';
+import TypeFilter from '@/components/local/filter/TypeFilter';
+import { SearchData, useSearch } from '@/contexts/SearchContext';
+import { findCityNameByID } from '@/lib/utils';
+import { ArrowBigUpDash, Trash2 } from 'lucide-react';
+import React, { useEffect, useState } from 'react';
+import noTripFound from"@/assets/error-page-unscreen.gif"
 const staticItems = [
-  {
-    id: 'HÀNG ĐẦU',
-    label: 'Hàng đầu'
-  },
-  {
-    id: 'HÀNG GIỮA',
-    label: 'Hàng giữa'
-  },
-  {
-    id: 'HÀNG SAU',
-    label: 'Hàng cuối'
-  },
+  { id: 'HÀNG ĐẦU', label: 'Hàng đầu' },
+  { id: 'HÀNG GIỮA', label: 'Hàng giữa' },
+  { id: 'HÀNG SAU', label: 'Hàng cuối' },
   { label: 'Giờ sớm nhất', id: 'THỜI GIAN ĐI SỚM NHẤT' },
   { label: 'Giờ muộn nhất', id: 'THỜI GIAN ĐI MUỘN NHẤT' },
   { label: 'Đánh giá tăng dần', id: 'TỔNG SỐ ĐÁNH GIÁ TĂNG DẦN' },
   { label: 'Đánh giá giảm dần', id: 'TỔNG SỐ ĐÁNH GIÁ GIẢM DẦN' },
   { label: 'Giá tăng dần', id: 'GIÁ TĂNG DẦN' },
   { label: 'Giá giảm dần', id: 'GIÁ GIẢM DẦN' }
-] as const
+] as const;
+
 function SearchPage() {
-  const [showScrollButton, setShowScrollButton] = useState(false)
-  const { data: companies } = useGetCompanies()
+  const [showScrollButton, setShowScrollButton] = useState(false);
+  const { data: companies } = useGetCompanies();
   const companyItems = companies
     ? companies.map((company) => ({
         id: company.CompanyID,
         label: company.Name
       }))
-    : []
+    : [];
 
-  const filterItems: readonly { id: string; label: string }[] = [...staticItems, ...companyItems]
-  const { searchData } = useSearch()
+  const filterItems: readonly { id: string; label: string }[] = [...staticItems, ...companyItems];
+  const { searchData } = useSearch();
   const initialState = {
     sortOption: 'DEFAULT',
     sortCompany: [] as string[],
     seatAvailability: [] as string[]
-  }
-  const [filterState, setFilterState] = useState(initialState)
-  const [currentPage, setCurrentPage] = useState<number>(1)
-  const { data, isFetching, refetch } = useGetTripSearchForm(searchData, filterState, currentPage)
-  console.log('data ở searchPage', data)
-  const { data: dataCityFromTo } = useGetCitySearchForm()
-  console.log(data)
+  };
+  const [filterState, setFilterState] = useState(initialState);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const { data, isFetching, refetch } = useGetTripSearchForm(searchData, filterState, currentPage);
+  const { data: dataCityFromTo } = useGetCitySearchForm();
 
   const handleSortOptionChange = (value: string) => {
     setFilterState((prevState) => ({
       ...prevState,
       sortOption: value
-    }))
-    setCurrentPage(1) // Reset current page to 1
-  }
+    }));
+    setCurrentPage(1); // Reset current page to 1
+  };
 
   const handleSortCompanyChange = (items: string[]) => {
     setFilterState((prevState) => ({
       ...prevState,
       sortCompany: items
-    }))
-    setCurrentPage(1) // Reset current page to 1
-  }
+    }));
+    setCurrentPage(1); // Reset current page to 1
+  };
+
   const handleSeatAvailabilityChange = (items: string[]) => {
     setFilterState((prevState) => ({
       ...prevState,
       seatAvailability: items
-    }))
-    setCurrentPage(1) // Reset current page to 1
-  }
-  console.log('filter ne', filterState)
+    }));
+    setCurrentPage(1); // Reset current page to 1
+  };
+
   const handleClearFilters = () => {
-    setFilterState(initialState)
-    setCurrentPage(1) // Reset current page to 1
-  }
+    setFilterState(initialState);
+    setCurrentPage(1); // Reset current page to 1
+  };
+
   const handleSearchSubmit = (values: SearchData) => {
-    setFilterState(initialState)
-    setCurrentPage(1)
-    refetch()
-  }
+    setFilterState(initialState);
+    setCurrentPage(1);
+    refetch();
+  };
+
   const handleScrollToTop = (e: React.MouseEvent) => {
-    e.preventDefault()
-    window.scrollTo({ top: 0, behavior: 'smooth' })
-  }
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 100) {
-        setShowScrollButton(true)
+        setShowScrollButton(true);
       } else {
-        setShowScrollButton(false)
+        setShowScrollButton(false);
       }
-    }
+    };
 
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
-  // if (isPending) return <Loading />
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   useEffect(() => {
     // Only refetch data if filterState has changed
-      refetch()
-  }, [filterState, refetch])
+    refetch();
+  }, [filterState, refetch]);
+
   const renderPagination = () => {
     if (!data || data.TotalCount === 0) {
       return null;
     }
-  
+
     const totalPages = data.TotalCount; // Assuming TotalCount represents the total number of pages
-  
+
     // Create an array of page numbers
     const pages = [];
     for (let i = 1; i <= totalPages; i++) {
       pages.push(i);
     }
-  
+
     return (
       <div className='flex justify-center my-4 space-x-2'>
         {pages.map((page) => (
@@ -137,93 +133,60 @@ function SearchPage() {
       </div>
     );
   };
+
   return (
     <div className='w-screen flex justify-center items-center bg-secondary pb-12'>
-      <div className='flex flex-col justify-center items-center w-2/3 '>
+      <div className='flex flex-col justify-center items-center w-2/3'>
         <div className='w-full flex justify-center absolute top-[100px]'>
           <SearchForm onsubmitSearch={handleSearchSubmit} />
         </div>
-        {/* <h1 className='mt-52 mb-4 text-4xl font-bold'>{searchData.startLocation} - {searchData.endLocation}</h1> */}
-        {isFetching ? (
-          <div className='mt-40'>
-            <Loading />
-
-            <p className='mt-3 animate-pulse'>Đang tìm kiếm chuyến xe, bạn vui lòng đợi chút xíu...</p>
+        <h1 className='mt-56 mb-8 text-3xl font-bold text-center'>
+          {findCityNameByID(searchData.startLocation, dataCityFromTo?.FromCities || [])} -{' '}
+          {findCityNameByID(searchData.endLocation, dataCityFromTo?.ToCities || [])}
+        </h1>
+        <div className='flex w-full gap-5 main' id='result'>
+          <div className='sticky top-24 slidebar flex flex-col shadow-md border rounded-lg bg-white w-2/5 h-fit'>
+            <div className='flex justify-between items-center gap-5 py-2 text-md font-bold pr-2'>
+              <p className='ml-4'>Bộ lọc tìm kiếm</p>
+              <p
+                className='flex text-red-500 cursor-pointer justify-center items-center gap-2 px-2 py-1 rounded-md hover:bg-secondary'
+                onClick={handleClearFilters}
+              >
+                Bỏ lọc <Trash2 />
+              </p>
+            </div>
+            <Arrange selectedValue={filterState.sortOption} onValueChange={handleSortOptionChange} />
+            <BusFilter selectedItems={filterState.sortCompany} onItemsChange={handleSortCompanyChange} />
+            <TypeFilter selectedItems={filterState.seatAvailability} onItemsChange={handleSeatAvailabilityChange} />
           </div>
-        ) : (
-          <>
-            <h1 className='mt-56  mb-8 text-3xl font-bold text-center '>
-              {findCityNameByID(searchData.startLocation, dataCityFromTo?.FromCities || [])} -{' '}
-              {findCityNameByID(searchData.endLocation, dataCityFromTo?.ToCities || [])}
-            </h1>
-            <div className='flex w-full gap-5 main ' id='result'>
-              <div className='sticky top-24 slidebar flex flex-col shadow-md border rounded-lg bg-white w-2/5 h-fit'>
-                <div className='flex justify-between items-center gap-5 py-2 text-md font-bold pr-2 '>
-                  <p className='ml-4'>Bộ lọc tìm kiếm</p>
-                  <p
-                    className='flex text-red-500 cursor-pointer justify-center items-center gap-2 px-2 py-1 rounded-md hover:bg-secondary'
-                    onClick={handleClearFilters}
-                  >
-                    Bỏ lọc
-                    <Trash2 />
-                  </p>
-                </div>
-                <Arrange selectedValue={filterState.sortOption} onValueChange={handleSortOptionChange} />
-                <BusFilter selectedItems={filterState.sortCompany} onItemsChange={handleSortCompanyChange} />
-                <TypeFilter selectedItems={filterState.seatAvailability} onItemsChange={handleSeatAvailabilityChange} />
+
+          <div className='w-full flex flex-col'>
+            <div className='flex'>
+              <BadgeList items={filterItems} selectedItems={filterState.sortCompany} onItemsChange={handleSortCompanyChange} />
+              <BadgeList items={filterItems} selectedItems={filterState.seatAvailability} onItemsChange={handleSeatAvailabilityChange} />
+            </div>
+            {isFetching ? (
+              <div className='mt-40'>
+                <Loading />
+                <p className='mt-3 animate-pulse'>Đang tìm kiếm chuyến xe, bạn vui lòng đợi chút xíu...</p>
               </div>
-
-              <div className='w-full flex flex-col'>
-                <div className='flex'>
-                  <BadgeList
-                    items={filterItems}
-                    selectedItems={filterState.sortCompany}
-                    onItemsChange={handleSortCompanyChange}
-                  />
-                  <BadgeList
-                    items={filterItems}
-                    selectedItems={filterState.seatAvailability}
-                    onItemsChange={handleSeatAvailabilityChange}
-                  />
-                </div>
-
+            ) : (
+              <>
                 {data && data.Items.length > 0 ? (
                   data.Items.map((item, index) => <CardTrip key={index} data={item} />)
                 ) : (
-                  <>
-                    <img></img>
-                    <h1 className='mt-40 font-semibold text-center'>
-                      <>
-                        Không tìm thấy chuyến xe.
-                        <p className='text-center'>Xin bạn vui lòng thay đổi tuyến đường tìm kiếm hoặc bỏ lọc!</p>
-                      </>
+                  <div className='flex flex-col items-center'>
+                    <img src={noTripFound} alt='No trips found' className='w-96 mb-4' />
+                    <h1 className='font-semibold text-center text-lg'>
+                      Không tìm thấy chuyến xe. Xin bạn vui lòng thay đổi tuyến đường tìm kiếm hoặc bỏ lọc!
                     </h1>
-                  </> // Replace with your default CardTrip component
+                  </div>
                 )}
-                {/* {data?.Items.map((item, index) => <CardTrip key={index} data={item} />)} */}
-                {/* {data && (
-                  <div className='flex justify-center my-4'>
-                  <button
-                    className={`px-4 py-2 mr-2 rounded bg-primary text-white ${currentPage === 1 ? 'opacity-70 cursor-not-allowed' : '  hover:bg-primary-dark'}`}
-                    onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-                    disabled={currentPage === 1}
-                  >
-                    Previous
-                  </button>
-                  <button
-                    className={`px-4 py-2 ml-2 rounded bg-primary text-white ${data?.TotalCount === currentPage ? 'opacity-70 cursor-not-allowed' : '  hover:bg-primary-dark'}`}
-                    onClick={() => setCurrentPage((prev) => prev + 1)}
-                    disabled={data?.TotalCount === currentPage}
-                  >
-                    Next
-                  </button>
-                </div>
-                )} */}
-                 {renderPagination()}
-              </div>
-            </div>
-          </>
-        )}
+                {renderPagination()}
+              </>
+            )}
+          </div>
+        </div>
       </div>
 
       {showScrollButton && (
@@ -231,14 +194,14 @@ function SearchPage() {
           <a
             href='#result'
             onClick={handleScrollToTop}
-            className='sticky top-3/4 ml-10 bg-primary rounded-full text-white flex justify-center items-center p-2  mb-4 transition duration-300'
+            className='sticky top-3/4 ml-10 bg-primary rounded-full text-white flex justify-center items-center p-2 mb-4 transition duration-300'
           >
             <ArrowBigUpDash size={30} fill='white' />
           </a>
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default SearchPage
+export default SearchPage;
